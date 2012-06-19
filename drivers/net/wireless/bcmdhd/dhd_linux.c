d0c1189eeab251d1c9aa49484347ac35d7469bf8
stratosk <stratosk@albert.(none)>
2012-06-20 00:52:32 +0300

    WiFi PM_MAX when suspend

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -526,6 +526,12 @@ static void dhd_set_packet_filter(int value, dhd_pub_t *dhd)
 #endif
 }
 
+#if defined(CONFIG_HAS_EARLYSUSPEND)
+#ifdef CONFIG_BCMDHD_WIFI_PM
+static int wifi_pm = 0;
+/* /sys/module/bcmdhd/parameters/wifi_pm */
+module_param(wifi_pm, int, 0644);
+#endif
 static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 {
 	int power_mode = PM_MAX;
@@ -537,7 +543,11 @@ static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 	DHD_TRACE(("%s: enter, value = %d in_suspend=%d\n",
 		__FUNCTION__, value, dhd->in_suspend));
 
-	dhd_suspend_lock(dhd);
+#ifdef CONFIG_BCMDHD_WIFI_PM
+	if (wifi_pm == 1)
+	    power_mode = PM_FAST;
+#endif
+
 	if (dhd && dhd->up) {
 		if (value && dhd->in_suspend) {
 
